23347c41849ce031c204631dd0ae56f457c04ec0
Carolina Armentano <ana.asilva@me.com.br>
2024-04-02 12:12:38 -0300

    refactor: prevent resource leak reading file.

--- src/main/java/br/ucsal/certificateGenerator/application/ParticipanteService.java
+++ src/main/java/br/ucsal/certificateGenerator/application/ParticipanteService.java
@@ -1,6 +1,7 @@
 package br.ucsal.certificateGenerator.application;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,42 +14,48 @@ import org.apache.poi.ss.usermodel.Row;
 import br.ucsal.certificateGenerator.domain.Participante;
 
 public class ParticipanteService {
-	
-	public List<Participante> lerPlanilhaDeParticipantes(FileInputStream fis, String nomeEvento, int cargaHorariaEvento) {
-		List<Participante> listaParticipantes = new ArrayList<>();
-        try {
-            HSSFWorkbook planilha = new HSSFWorkbook(fis);
-            HSSFSheet participantes = planilha.getSheetAt(0);
-            Iterator<Row> itLinha = participantes.iterator();
-            // Passando Pelas Linhas
-            while (itLinha.hasNext()) {
-                Row linha = itLinha.next();
-                Iterator<Cell> celIt = linha.cellIterator();
-                String nome = "";
-                String cpf = "";
-                String email = "";
-                // Passando pelas Colunas
-                while (celIt.hasNext()) {
-                    Cell celula = celIt.next();
-                    switch (celula.getColumnIndex()) {
-                        case 0:
-                            nome = celula.getStringCellValue();
-                            break;
-                        case 1:
-                            cpf = String.valueOf((long) celula.getNumericCellValue());
-                            break;
-                        case 2:
-                            email = celula.getStringCellValue();
-                            break;
-                    }
-                }
-                listaParticipantes.add(new Participante(nome, cpf, email, nomeEvento, cargaHorariaEvento));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        return listaParticipantes;
-	}
 
+	public List<Participante> lerPlanilhaDeParticipantes(FileInputStream fis, String nomeEvento,
+			int cargaHorariaEvento) {
+		List<Participante> listaParticipantes = new ArrayList<>();
+		try (HSSFWorkbook planilha = new HSSFWorkbook(fis)) {
+			HSSFSheet participantes = planilha.getSheetAt(0);
+			Iterator<Row> itLinha = participantes.iterator();
+			// Passando Pelas Linhas
+			while (itLinha.hasNext()) {
+				Row linha = itLinha.next();
+				Iterator<Cell> celIt = linha.cellIterator();
+				String nome = "";
+				String cpf = "";
+				String email = "";
+				// Passando pelas Colunas
+				while (celIt.hasNext()) {
+					Cell celula = celIt.next();
+					switch (celula.getColumnIndex()) {
+					case 0:
+						nome = celula.getStringCellValue();
+						break;
+					case 1:
+						cpf = String.valueOf((long) celula.getNumericCellValue());
+						break;
+					case 2:
+						email = celula.getStringCellValue();
+						break;
+					}
+				}
+				listaParticipantes.add(new Participante(nome, cpf, email, nomeEvento, cargaHorariaEvento));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return listaParticipantes;
+	}
 }
