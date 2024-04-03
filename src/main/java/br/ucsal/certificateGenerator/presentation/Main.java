d5751a7e7440823e0741e55891a6e53138d6114d
Carolina Armentano <ana.asilva@me.com.br>
2024-04-03 17:16:26 -0300

    feat: add progress bar and centralize items in component.

--- src/main/java/br/ucsal/certificateGenerator/presentation/Main.java
+++ src/main/java/br/ucsal/certificateGenerator/presentation/Main.java
@@ -1,15 +1,14 @@
 package br.ucsal.certificateGenerator.presentation;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -22,63 +21,85 @@ import br.ucsal.certificateGenerator.domain.Participante;
 
 public class Main extends Application {
 
-	private final FileChooser fileChooser = new FileChooser();
+    private final FileChooser fileChooser = new FileChooser();
+    private ProgressBar progressBar = new ProgressBar(0);
+    private final int HEIGHT = 600;
+    private final int WIDTH = 400;
+    private final int PROGRESS_BAR_WIDTH = 300;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        TextField nomeEventoField = new TextField("Nome do Evento");
+        TextField cargaHorariaField = new TextField("Carga Horaria");
+        Button submitButton = new Button("Anexar Planilha e Enviar E-mails");
+        TextArea outputTextArea = new TextArea();
+        outputTextArea.setEditable(false);
+        setupFileChooser();
 
-	@Override
-	public void start(Stage primaryStage) {
-		TextField nomeEventoField = new TextField("Nome do Evento");
-		TextField cargaHorariaField = new TextField("Carga Horaria");
-		Button submitButton = new Button("Anexar Planilha e Enviar E-mails");
-		TextArea outputTextArea = new TextArea();
-		outputTextArea.setEditable(false);
-		setupFileChooser();
+        progressBar.setPrefWidth(PROGRESS_BAR_WIDTH);
 
-		submitButton.setOnAction(e -> {
-			try {
-				outputTextArea.appendText("\nProcessando... Aguarde");
-				String nomeEvento = nomeEventoField.getText();
-				String cargaHoraria = cargaHorariaField.getText();
-				File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        submitButton.setOnAction(e -> {
+            File selectedFile = fileChooser.showOpenDialog(primaryStage);
+            if (selectedFile == null) {
+                return;
+            }
 
-				if (nomeEvento.isEmpty() || nomeEvento.equals("Nome do Evento") || cargaHoraria.isEmpty() || selectedFile == null)
-					throw new IllegalArgumentException("Por favor, preencha todos os campos corretamente e selecione uma planilha.");
+            String nomeEvento = nomeEventoField.getText();
+            String cargaHoraria = cargaHorariaField.getText();
 
-				int cargaHorariaEvento = Integer.parseInt(cargaHoraria);
-				
-				ParticipanteService participanteService = new ParticipanteService();
-				
-				List<Participante> listaParticipantes = participanteService.lerPlanilhaDeParticipantes(selectedFile,
-						nomeEvento, cargaHorariaEvento);
-				
-				CertificateGeneratorService certificateGeneratorService = new CertificateGeneratorService(listaParticipantes);
-				listaParticipantes = certificateGeneratorService.gerarCertificados();
-				
-				EmailService emailService = new EmailService();
-				String result = emailService.enviarEmails(listaParticipantes);
-				String message = "\nRELATÓRIO DE ENVIO DE CERTIFICADOS \nNome do Evento: " + nomeEvento + ", Carga Horária: " + cargaHoraria + "\n"
-						+ result;
-				outputTextArea.appendText(message);
-			} catch (Exception ex) {
-				String message = "[ERRO] " + ex.getMessage();
-				System.err.println(message);
-				outputTextArea.appendText("\n" + message);
-			}
-		});
+            if (nomeEvento.isEmpty() || nomeEvento.equals("Nome do Evento") || cargaHoraria.isEmpty()) {
+                outputTextArea.appendText("\nPor favor, preencha todos os campos corretamente.");
+                return;
+            }
 
-		VBox root = new VBox(10, nomeEventoField, cargaHorariaField, submitButton, outputTextArea);
-		Scene scene = new Scene(root, 600, 400);
+            int cargaHorariaEvento = Integer.parseInt(cargaHoraria);
 
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("Certificate Generator");
-		primaryStage.show();
-	}
+            Task<Void> task = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    ParticipanteService participanteService = new ParticipanteService();
+                    List<Participante> listaParticipantes = participanteService.lerPlanilhaDeParticipantes(selectedFile,
+                            nomeEvento, cargaHorariaEvento);
+                    updateProgress(1, 3);
 
-	private void setupFileChooser() {
-		fileChooser.setTitle("Anexar Planilha");
-		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Planilhas", "*.xls", "*.xlsx"));
-	}
+                    CertificateGeneratorService certificateGeneratorService = new CertificateGeneratorService(listaParticipantes);
+                    listaParticipantes = certificateGeneratorService.gerarCertificados();
+                    updateProgress(2, 3);
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+                    EmailService emailService = new EmailService();
+                    String result = emailService.enviarEmails(listaParticipantes);
+                    updateProgress(3, 3);
+
+                    String message = "\nRELATÓRIO DE ENVIO DE CERTIFICADOS \nNome do Evento: " + nomeEvento + ", Carga Horária: " + cargaHoraria + "\n"
+                            + result;
+                    outputTextArea.appendText(message);
+                    return null;
+                }
+            };
+
+            progressBar.progressProperty().bind(task.progressProperty());
+
+            Thread thread = new Thread(task);
+            thread.setDaemon(true);
+            thread.start();
+        });
+
+        VBox root = new VBox(10, nomeEventoField, cargaHorariaField, submitButton, outputTextArea, progressBar);
+        root.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(root, HEIGHT, WIDTH);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Certificate Generator");
+        primaryStage.show();
+    }
+
+    private void setupFileChooser() {
+        fileChooser.setTitle("Anexar Planilha");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Planilhas", "*.xls", "*.xlsx"));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
